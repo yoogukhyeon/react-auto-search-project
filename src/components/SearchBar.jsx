aae5a8aafd31ba1a3240a7735df549d32a924fab
yoogukhyeon <rnrgus5897@gmail.com>
2024-01-26 14:43:40 +0900

    commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((val) => {
+          return val.title.toLowerCase().includes(value);
+        });
+        setResults(results);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type to search..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
